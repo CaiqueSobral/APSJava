3b650db829f9a49b16536550954faa2199c22684
KykyLS <51248056+KykyLS@users.noreply.github.com>
2019-11-03 20:28:20 -0300

    Add files via upload

---
+++ APSdeJava/src/Principal/Conta.java
@@ -0,0 +1,79 @@
+package Principal;
+
+import javax.swing.JOptionPane;
+public class Conta {
+    
+    private int numero;
+    
+    //String só para testes
+    private String cliente;
+    //-------------
+    private String DataAbertura;
+    protected double saldo;
+
+    public int getNumero() {
+        return numero;
+    }
+
+    public void setNumero(int numero) {
+        this.numero = numero;
+    }
+
+    public String getCliente() {
+        return cliente;
+    }
+
+    public void setCliente(String cliente) {
+        this.cliente = cliente;
+    }
+
+    public String getDataAbertura() {
+        return DataAbertura;
+    }
+
+    public void setDataAbertura(String DataAbertura) {
+        this.DataAbertura = DataAbertura;
+    }
+
+    public double getSaldo() {
+        return saldo;
+    }
+
+    public void setSaldo(double saldo) {
+        this.saldo = saldo;
+    }
+    
+    public void depositar(Double quantia){
+        
+        this.setSaldo(this.saldo + quantia);
+        
+    }
+    
+    public boolean debitar(Double quantia){
+        boolean debito = false;
+        
+        if (this.saldo < quantia){
+            
+            debito = true;
+            this.setSaldo(this.saldo - quantia);
+        }
+        if (debito = true){
+            JOptionPane.showMessageDialog(null, "Débito confirmado");
+        }else{
+            JOptionPane.showMessageDialog(null, "Débito negado, Saldo inválido");
+        }
+        return debito;
+    }
+    
+    
+    public boolean transferir(int numero, Double quantia){
+        boolean transferido = false;
+        
+        return transferido;
+    }
+    
+    public void imprimir(){
+        
+        
+    }
+}
---
+++ APSdeJava/src/Principal/ContaEspecial.java
@@ -0,0 +1,32 @@
+package Principal;
+
+public class ContaEspecial extends Conta{
+    
+    private double limite;
+
+    public double getLimite() {
+        return limite;
+    }
+
+    public void setLimite(double limite) {
+        this.limite = limite;
+    }
+    
+    public boolean debitar(Double quantia){
+        boolean debito = false;
+        Double saldoAtual = this.saldo;
+        
+        if (quantia <= limite - saldoAtual){
+            debito = true;
+            
+            this.saldo = saldoAtual - quantia;
+        }
+               
+        return debito;
+    }
+    
+    public void imprimir(){
+        
+    }
+    
+}
